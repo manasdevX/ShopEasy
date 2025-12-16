d7baa6dce66b1c59eb72bdfaadd1ced356962808
Manas-Agnihotri-MNNIT <agnihotrimanas99@gmail.com>
2025-12-17 01:45:50 +0530

    Backend: initial server, product model, routes and controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
-const app = require("./app");
-const connectDB = require("./config/db");
-require("dotenv").config();
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
