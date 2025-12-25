0a8973ad91d7ca87ec8b4c119229c8063668e4cf
Manas-Agnihotri-MNNIT <agnihotrimanas99@gmail.com>
2025-12-26 03:08:31 +0530

    add health endpoint for uptimebot

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,11 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
