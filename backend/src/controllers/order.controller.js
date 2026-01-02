a8ccb7324385aae03e8c6bec447dea80c490d229
Manas Agnihotri <agnihotrimanas99@gmail.com>
2026-01-02 20:38:48 +0530

    Update order.controller.js

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,7 +1,7 @@
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
 import { createNotification } from "./notification.controller.js";
-import sendEmail from "../utils/sendEmail.js";
+import sendEmail from "../utils/emailHelper.js";
 
 // @desc    Create new order (Direct COD or General)
 // @route   POST /api/orders
