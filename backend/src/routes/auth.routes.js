2a4fa118818b24e11e47017581c932eb657314c1
Manas Agnihotri <agnihotrimanas99@gmail.com>
2026-01-03 03:26:57 +0530

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -12,7 +12,7 @@ import {
 } from "../controllers/auth.controller.js";
 
 import { getMe } from "../controllers/auth.controller.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
